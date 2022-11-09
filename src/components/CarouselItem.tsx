1bd47744eb1da302c8615e664a64dd5245badc47
Takodadon <lhanhsin@gmail.com>
2022-11-10 03:19:22 +1100

    Update CarouselItem.tsx

--- src/components/CarouselItem.tsx
+++ src/components/CarouselItem.tsx
@@ -111,19 +111,20 @@ export const CarouselItem = ({ animation, next, prev, swipe, state, index, maxIn
     let animate = 'center';
     if (index === active)
         animate = 'center';
-    else if (maxIndex>1 && index === prevActive)
+    else if (maxIndex < 2 || index === prevActive)
     {
         animate = isNext ? 'leftwardExit' : 'rightwardExit';
-        if (active === maxIndex && index === 0) animate = 'rightwardExit';
-        if (active === 0 && index === maxIndex) animate = 'leftwardExit'
+        if (maxIndex > 1) {
+            if (active === maxIndex && index === 0) animate = 'rightwardExit';
+            if (active === 0 && index === maxIndex) animate = 'leftwardExit'
+        }
     }
     else
     {
         animate = index < active ? 'leftOut' : 'rightOut';
-        if (maxIndex > 1) {
-            if (active === maxIndex && index === 0) animate = 'rightOut';
-            if (active === 0 && index === maxIndex) animate = 'leftOut';
-        }
+        if (active === maxIndex && index === 0) animate = 'rightOut';
+        if (active === 0 && index === maxIndex) animate = 'leftOut';
+        
     }
 
     duration = duration / 1000;
